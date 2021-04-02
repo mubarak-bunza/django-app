36f5f03529f842f55f6df75739fc3c58efe06090
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-04-02 18:50:51 +0100

    edit view

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "message": "Hello World!"
+        "message": "Hello World! edited"
     }
     return render(request, 'hello_world/index.html', context)
