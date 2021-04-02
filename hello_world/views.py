178d12a391f87994d2a51275c964b779647962f8
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-04-02 19:24:02 +0100

    testing manual review

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "message": "Hello World! edited"
+        "message": "Hello World! added manual review"
     }
     return render(request, 'hello_world/index.html', context)
